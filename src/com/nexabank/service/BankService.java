87c4d33aa23358a33021c2807e9e02a38e310e5d
Parlog07 <ayoubmogador2014@gmail.com>
2026-09-13 22:13:22 +0100

    Update Bank Service

--- src/com/nexabank/service/BankService.java
+++ src/com/nexabank/service/BankService.java
@@ -6,20 +6,27 @@ import com.nexabank.model.Account;
 import com.nexabank.model.Transaction;
 import com.nexabank.model.TransactionType;
 import com.nexabank.utils.StatementFileManager;
-import java.io.IOException;
 
+import java.io.IOException;
 import java.util.UUID;
 
 public class BankService {
+
     private StatementFileManager statementFileManager = new StatementFileManager();
 
-    public void deposit(Account account, double amount) throws InvalidAmountException {
+
+    public void deposit(Account account, double amount)
+            throws InvalidAmountException, IOException {
 
         if (amount <= 0) {
-            throw new InvalidAmountException("Amount must be greater than 0");
+            throw new InvalidAmountException(
+                    "Amount must be greater than 0"
+            );
         }
 
-        account.setBalance(account.getBalance() + amount);
+        account.setBalance(
+                account.getBalance() + amount
+        );
 
         Transaction transaction = new Transaction(
                 UUID.randomUUID().toString(),
@@ -30,21 +37,34 @@ public class BankService {
         );
 
         account.addTransaction(transaction);
-        statementFileManager.saveTransaction(account, transaction);
+
+        statementFileManager.saveTransaction(
+                account,
+                transaction
+        );
     }
 
+
     public void withdraw(Account account, double amount)
-            throws InvalidAmountException, InsufficientBalanceException {
+            throws InvalidAmountException,
+            InsufficientBalanceException,
+            IOException {
 
         if (amount <= 0) {
-            throw new InvalidAmountException("Amount must be greater than 0");
+            throw new InvalidAmountException(
+                    "Amount must be greater than 0"
+            );
         }
 
         if (amount > account.getBalance()) {
-            throw new InsufficientBalanceException("Insufficient balance");
+            throw new InsufficientBalanceException(
+                    "Insufficient balance"
+            );
         }
 
-        account.setBalance(account.getBalance() - amount);
+        account.setBalance(
+                account.getBalance() - amount
+        );
 
         Transaction transaction = new Transaction(
                 UUID.randomUUID().toString(),
@@ -55,22 +75,41 @@ public class BankService {
         );
 
         account.addTransaction(transaction);
-        statementFileManager.saveTransaction(account, transaction);
+
+        statementFileManager.saveTransaction(
+                account,
+                transaction
+        );
     }
 
-    public void transfer(Account sourceAccount, Account destinationAccount, double amount)
-            throws InvalidAmountException, InsufficientBalanceException {
+
+    public void transfer(
+            Account sourceAccount,
+            Account destinationAccount,
+            double amount
+    ) throws InvalidAmountException,
+            InsufficientBalanceException,
+            IOException {
 
         if (amount <= 0) {
-            throw new InvalidAmountException("Amount must be greater than 0");
+            throw new InvalidAmountException(
+                    "Amount must be greater than 0"
+            );
         }
 
         if (amount > sourceAccount.getBalance()) {
-            throw new InsufficientBalanceException("Insufficient balance");
+            throw new InsufficientBalanceException(
+                    "Insufficient balance"
+            );
         }
 
-        sourceAccount.setBalance(sourceAccount.getBalance() - amount);
-        destinationAccount.setBalance(destinationAccount.getBalance() + amount);
+        sourceAccount.setBalance(
+                sourceAccount.getBalance() - amount
+        );
+
+        destinationAccount.setBalance(
+                destinationAccount.getBalance() + amount
+        );
 
         Transaction transaction = new Transaction(
                 UUID.randomUUID().toString(),
@@ -82,5 +121,15 @@ public class BankService {
 
         sourceAccount.addTransaction(transaction);
         destinationAccount.addTransaction(transaction);
+
+        statementFileManager.saveTransaction(
+                sourceAccount,
+                transaction
+        );
+
+        statementFileManager.saveTransaction(
+                destinationAccount,
+                transaction
+        );
     }
 }
